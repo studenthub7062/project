80b38191f1c8573ed2fa0d058ee17d9ee1b40128
studenthub7062 <studenthub7062@gmail.com>
2023-11-07 10:22:37 +0530

    jhfjdk

---
+++ app.js
@@ -0,0 +1,58 @@
+// const post ={
+//     username:"@ibrahim",
+//     content:"this is my #Firstpost",
+//     likes:150,
+//     reports:6,
+//     tags:["@JAHIR MEDIA","@DELTA"]
+// };;
+
+
+// const obj = {
+//     1:"a",
+//     2:"b",
+//     3:"c",
+//     4:"d",
+//     undefined:"e"
+// };
+
+// const student ={
+//     name:"Ibrahim",
+//     age:21,
+//     marks:70.6,
+//     city:"Giridih"
+// };
+
+// const classInfo = {
+//     Ibrahim:{
+//         grade:"A+",
+//         city:"Giridih"
+//     },
+//     Khushboo:{
+//         grade:"A",
+//         city:"Birni"
+//     },
+//     Parwej:{
+//         grade:"b",
+//         city:"Urro"
+//     }
+
+// };
+
+const classInfo = [
+    {
+        name:"Ibrahim",
+        grade:"A+",
+        city:"mumbai"
+    },
+    {
+        name:"rani",
+        grade:"A",
+       city:"Birni"
+
+    },
+    {
+        name:"golu",
+        grade:"A",
+        city:"suriya"
+    }
+];
